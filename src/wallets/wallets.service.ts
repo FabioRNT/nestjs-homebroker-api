a71ad421b0655687d44da2df81badc44cdbccddf
Fabio Ribeiro <fabio.ribeiro@koneksys.com>
2025-02-19 10:10:27 -0300

    Add Wallets module with controller, service, and DTOs

---
+++ src/wallets/wallets.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+import { Model } from 'mongoose';
+import { Wallet } from './entities/wallet.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { WalletAsset } from './entities/wallet-asset.entity';
+
+@Injectable()
+export class WalletsService {
+  constructor(@InjectModel(Wallet.name) private walletSchema: Model<Wallet>,
+    @InjectModel(WalletAsset.name)
+    private walletAssetSchema: Model<WalletAsset>,
+  ) {}
+
+  create(createWalletDto: CreateWalletDto) {
+    return this.walletSchema.create(createWalletDto);
+  }
+
+  findAll() {
+    return this.walletSchema.find();
+  }
+
+  findOne(id: string) {
+    return this.walletSchema.findById(id);
+  }
+
+  createWalletAsset(data: {
+    walletId: string;
+    assetId: string;
+    shares: number;
+  }) {
+    return this.walletAssetSchema.create({
+      walletId: data.walletId,
+      assetId: data.assetId,
+      shares: data.shares,
+    });
+  }
+}

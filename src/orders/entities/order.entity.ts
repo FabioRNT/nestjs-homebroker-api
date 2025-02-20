901d7eb107568f350b478fdad14bef4263046d37
Fabio Ribeiro <fabio.ribeiro@koneksys.com>
2025-02-20 20:34:28 -0300

    Refactor orders and wallets modules to include presenters for improved data representation; update asset creation example in API documentation.

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import crypto from 'crypto';
 import { Asset, AssetDocument } from 'src/assets/entities/asset.entity';
 import { Wallet, WalletDocument } from 'src/wallets/entities/wallet.entity';
 
